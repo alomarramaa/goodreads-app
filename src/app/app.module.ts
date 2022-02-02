308120cd711a2fda778cb1695c10ad32e6d469a7
alomarramaa <alomar.ramaa@gmail.com>
2022-02-02 12:28:43 -0500

    Added 4 cards (3 books and 1 banner)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {TopNavBarComponent} from 'src/app/driver/topnavbar.component';
 import { AppComponent } from './app.component';
+import { CategoryCardComponent } from './categories/category.card.component';
 import { footerComponent } from './driver/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    footerComponent
+    footerComponent,
+    CategoryCardComponent
   ],
   imports: [
     BrowserModule
