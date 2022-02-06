b99c899afa7638f1ef3972cde20fd7a117dce685
alomarramaa <alomar.ramaa@gmail.com>
2022-02-06 14:31:35 -0500

    Added data attributes

--- src/app/books/vertical.card.component.ts
+++ src/app/books/vertical.card.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input } from "@angular/core";
 
 @Component({
-    selector:'app-vertical-card',
-    templateUrl:'vertical.card.component.html',
-    styleUrls:['vertical.card.component.css']
+    selector: 'app-vertical-card',
+    templateUrl: 'vertical.card.component.html',
+    styleUrls: ['vertical.card.component.css']
 })
-export class VerticalCardComponent{
-  
+export class VerticalCardComponent {
+    @Input() img: string;
+    @Input() link: string;
+
+    constructor() {
+        this.img = "";
+        this.link = "Missing link";
+    }
+
 }
