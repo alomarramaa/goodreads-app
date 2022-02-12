82528afc03d1f9af8a21e2ae35d652e72e31486d
alomarramaa <alomar.ramaa@gmail.com>
2022-02-12 13:55:23 -0500

    Changed background color of whole page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { mock_product_list } from './books/mock_product_list';
 import { ProductItemModel } from './books/product-item.model';
 import { category_list } from './categories/category_list';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+ 
 })
 export class AppComponent {
   title = 'goodreads-app';
