69c6ac5ad4bfdf1f2cbfd2b907d8d7aa0a8a406c
alomarramaa <alomar.ramaa@gmail.com>
2022-04-10 13:34:58 -0400

    Added auth service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,17 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
-    templateUrl:'auth.component.html',
-    styleUrls:['auth.component.css']
+    templateUrl:'auth.component.html'
 })
-export class AuthComponent{
-    public onSubmit(data:NgForm){
-        console.log("button clicked");
-        console.log(data);
-
+export class AuthComponent implements OnInit {
+    constructor(private auth:AuthService){}
+    ngOnInit(): void {
     }
-  
+
+public onSubmit(data:NgForm){
+    console.log("Button clicked");
+    console.log(data);
+    this.auth.signup(data.value.email, data.value.password).subscribe(
+        data => {
+            console.log(data);
+        }
+    );
+    data.reset();
+}
 
 }
+
+ 
